c6daa9e73c0fe275c4266013bd80ce533dbd2808
rogilmore <rog1@psu.edu>
2026-02-23 10:41:33 -0500

    Update reports; add functionality to investigators.qmd to save old data to separate directory.

---
+++ src/render.sh
@@ -0,0 +1,5 @@
+quarto render src/investigators.qmd --execute-param src/params_investigators.yml
+
+quarto render src/institutions.qmd --execute-param src/params_instituions.yml
+
+quarto render src/weekly.qmd --execute-param src/params_weekly.yml
