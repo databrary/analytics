591e8d917cd2048d19ae015e231550696398e4da
rogilmore <rog1@psu.edu>
2026-04-27 07:38:14 -0400

    Add export_reports_data.R script.

---
+++ export_reports_data.R
@@ -0,0 +1,18 @@
+# Gather data for reports
+fl <- list.files(file.path(here::here(), "R"), "*.R", 
+                 full.names = TRUE)
+purrr::map(fl, suppressPackageStartupMessages(source)) |>
+  purrr::quietly()
+
+# Login to Databrary
+databraryr::login_db(email = Sys.getenv("USERNAME"),
+                          password = Sys.getenv("PASSWORD"),
+                          client_id = Sys.getenv("CLIENT_ID"),
+                          client_secret = Sys.getenv("CLIENT_SECRET"),
+                          store = FALSE,
+                          overwrite = FALSE)
+
+export_institution_list()
+export_all_institution_investigators()
+export_all_institutions_sharing_data()
+
