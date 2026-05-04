c64612771b79d56cd65710eb7636456e6424237b
rogilmore <rog1@psu.edu>
2026-05-04 08:45:08 -0400

    Update packages and render.

--- export_reports_data.R
+++ export_reports_data.R
@@ -5,14 +5,19 @@ purrr::map(fl, suppressPackageStartupMessages(source)) |>
   purrr::quietly()
 
 # Login to Databrary
-databraryr::login_db(email = Sys.getenv("USERNAME"),
+logged_in <- databraryr::login_db(email = Sys.getenv("USERNAME"),
                           password = Sys.getenv("PASSWORD"),
                           client_id = Sys.getenv("CLIENT_ID"),
                           client_secret = Sys.getenv("CLIENT_SECRET"),
                           store = FALSE,
                           overwrite = FALSE)
 
-export_institution_list()
-export_all_institution_investigators()
-export_all_institutions_sharing_data()
+if (logged_in) {
+  export_institution_list()
+  export_all_institution_investigators()
+  export_all_institutions_sharing_data()  
+} else {
+  message("Unable to log in to Databrary.")
+}
+
 
